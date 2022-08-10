4e949102d309edfd9d7ffa831f6cc1482582bfe5
thuptm <thuptm@cystack.net>
2022-08-10 15:49:30 +0700

    add section23

---
+++ section19/application/flask_blog/config.py
@@ -0,0 +1,4 @@
+DEBUG = True
+SECRET_KEY = 'secret_key'
+USERNAME = 'minhthu'
+PASSWORD = 'thu123'
---
+++ section23/application/flask_blog/config.py
@@ -0,0 +1,6 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DEBUG = True
+SECRET_KEY = 'secret_key'
+USERNAME = 'minhthu'
+PASSWORD = 'thu123'
